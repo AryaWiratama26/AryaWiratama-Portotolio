3e7c46ae603c868bcd583ef80fa55ad14a1e8ba3
312310224 <154570729+312310224@users.noreply.github.com>
2024-01-07 11:41:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","Student","Gamer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
+var typed = new Typed(".about-text",{
+    strings:["Frontend Developer","Student","Gamer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
